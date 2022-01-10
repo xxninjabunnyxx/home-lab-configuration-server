381123867a5541be05387a3eb71254a12b6ed0cf
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2022-01-09 21:05:09 -0500

    first commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update
+
+sudo apt-get install avahi-daemon avahi-discover avahi-utils libnss-mdns mdns-scan
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo npm install -g pm2
+
+sudo apt-get install python3.6
+
+pip install ansible
+
+pm2 startup
+
+echo Run "pm2 start app.js --watch && pm2 save"\n
