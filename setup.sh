c8fed08e72cf89e60229d2b3f8dc9a77d264167e
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2022-01-28 18:06:54 -0500

    updated

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 
-sudo apt-get install avahi-daemon avahi-discover avahi-utils libnss-mdns mdns-scan
+sudo apt-get install avahi-daemon avahi-discover avahi-utils libnss-mdns mdns-scan ssh sshpass
 
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -9,6 +9,8 @@ sudo apt-get install -y nodejs
 sudo npm install -g pm2
 
 sudo apt-get install python3.6
+
+sudo adduser ansible
 
 pip install ansible
 
